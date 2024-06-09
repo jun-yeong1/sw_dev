1168540b8c5f5267feed5752747c10dc783c4cd3
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-06-09 22:10:30 +0900

    임시 그림 장바구니 수정
    그림 추가, 메뉴에 장바구니 같이

--- sw_project/routes/index.js
+++ sw_project/routes/index.js
@@ -13,27 +13,23 @@ router.post("/join", user.post_user);
 router.get("/login", user.login);
 router.post("/login", user.post_login);
 
+// edit 출력 확실시
+router.get("/edit", user.edit);
 router.post("/edit", user.edit);
 router.patch("/edit", user.patch_user);
 router.delete("/delete", user.delete_user);
 
-
 // 메뉴 + 장바구니
-router.get('/menu', cartController.getMenuPage);
+router.get('/menu', cartController.menu);
 router.get('/cart', cartController.getCartPage);
 router.post('/add-to-cart', cartController.addToCart);
 // 입금 페이지
-router.get('/user/add-money', auth, user.getAddMoneyPage);
-router.post('/user/add-money', auth, user.addMoney);
+router.get('/add-money', auth, user.getAddMoneyPage);
+router.post('/add-money', auth, user.addMoney);
 
 router.get("/logout", user.logout);
 
-// /main 페이지 경로 추가
-//router.get("/main", user.main);
-
-
 //map
-
 router.get('/map', Map.getMap);
 router.get('/api-key', Map.getApi);
 
